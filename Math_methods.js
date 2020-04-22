c2a7673251aaaff1146b10beb50b40d28cad77cc
Slyvochka <slivka.svitlana@gmail.com>
2020-04-22 01:27:30 -0400

    'ghjklkjh'

--- Math_methods.js
+++ Math_methods.js
@@ -12,3 +12,22 @@ function hypotenuse(a, b) {
 
 // функция hypotenuse(3, 4) должна возвратить 5;
 // функция hypotenuse(3, 5) должна возвратить 6.
+
+//  *******************************
+// function digits(num){
+//   let c = num % 10;
+//   num = (num - c) / 10;
+//   let b =num % 10;
+//   let a = (num - b) / 10;
+//   return a < b && b < c;
+// }
+
+//console.log(String(n)[0] !== String(n)[1] && String(n)[1] !== String(n)[2] && String(n)[2] !== String(n)[0])
+//-----------------------------
+//  function digits (num){
+//   let c = num % 10;
+//   let b = Math.trunc(num / 10) % 10;
+//   let a = Math.trunc(num / 100);
+// }
+// console.log(digits(457));
+//-----------------------------------------
