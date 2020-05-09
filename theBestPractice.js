b1ace7145abe9f4f9f3dfee1ec0c5c34c2af70a0
Slyvochka <slivka.svitlana@gmail.com>
2020-05-09 16:05:22 -0400

    liked exercises

---
+++ theBestPractice.js
@@ -0,0 +1,11 @@
+//               Exercise 1
+//  from JS Practice
+// Верно ли, что 0.1 + 0.2 === 0.3? Все не так просто!
+// функция isSumEqualsNumber(0.1, 0.2, 0.3) должна возвратить true.
+// Выполните округление суммы чисел и третьего числа до двух знаков в дробной части с помощью метода toFixed(n).
+
+
+// function isSumEqualsNumber(a, b, c){
+//     let d = (c + '').split('.');
+//       return (+(a + b).toFixed(d[1].length) === c );
+// }
