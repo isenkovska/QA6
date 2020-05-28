e3539c0c420f124a2de26a760a0707bf1f3f7ed4
Sviatoslav Naida <slivka.svitlana@gmail.com>
2020-05-28 12:51:09 -0400

    works with String

---
+++ String.js
@@ -0,0 +1,13 @@
+// *** Exercise 1
+// (Return the last word)
+// function letters(str){
+//     let res = '';
+//     for(let i = str.length - 1; i > 0; i--){
+//       if (str[i] !== ' ')
+//          res = str[i] + res;
+//       else 
+//         break;
+//     }
+//     return res;
+// }
+// console.log(letters("Hello world!")); // output "world!"
