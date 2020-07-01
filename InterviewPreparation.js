88ea23621356fefd6a9ce72b4208c1b6f5f654e5
slyvochka <slivka.svitlana@gmail.com>
2020-07-01 13:48:33 -0400

    Interview Preparation

---
+++ InterviewPreparation.js
@@ -0,0 +1,43 @@
+// https://docs.google.com/spreadsheets/d/19keEqFh_Z08_OgFpvBu7UWrmEGT5EYdqYaKSjhe2wM4/edit#gid=1948005648 Julia's test cases
+// https://github.com/jstepiko/CCA  
+// https://docs.google.com/document/d/10YxY2G35iNST-Ht1qWY-DWYRUiZtWlR4mFoE-cNZrjM/edit#heading=h.7zan7ymq7rce
+
+// *** 1 Exercise
+// We have two separate arrays, fill out with numbers,  and we need to make concatenation and descending sort .
+// const arr = [8, 7, 5, 3]; 
+// const arr1 = [2, 5, 30, 20, 25];
+  //---(Solution 1 with using methods )
+  //console.log([].concat(arr, arr1).sort((a, b) => b - a));
+   // --- (Soluton 2 with not using method concat() )
+    //   console.log((arr + ',' + arr1).split(',').sort((a,b) => b - a).map(el => el * 1));
+      // Solution with using only loops
+       //   const arr = [8, 7, 5, 3]; 
+       //   const arr1 = [2, 5, 30, 20, 25];
+       //     let l = arr.length;
+       //     for(let i = 0; i < arr1. length; i++){
+       //       if(!arr[l]){
+       //         arr[l] = arr1[i];
+       //         l++;
+       //       } 
+       //     }
+       //  //console.log(arr);
+       //  function bubbleSort(array) {
+       //    let done = false;
+       //    while (!done) {   
+       //      done = true;
+       //      for (var i = 0; i < array.length; i++) {
+       //        if (array[i + 1] > array[i]) {    
+       //          done = false;
+       //          var tmp = array[i + 1];      
+       //          array[i + 1] = array[i];        
+       //          array[i] = tmp;                
+       //        }
+       //      }
+       //    }
+       //    return array;
+       //  }
+       //  bubbleSort(arr);
+       //  console.log(bubbleSort(arr));      //Output [30, 25, 20, 8, 7, 5, 5, 3, 2]
+       
+     
+  
