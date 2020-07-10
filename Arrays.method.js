26c19748dfbdd1111c253d49e3f90d360a1a4a72
slyvochka <slivka.svitlana@gmail.com>
2020-07-10 18:12:21 -0400

    Arrays method

--- Arrays.method.js
+++ Arrays.method.js
@@ -232,7 +232,11 @@
 
 
 // --- Array.from() ---
-    //console.log(Array.from('foo'));
+  //let num = 123;
+  // console.log(Array.from(String(num)));          // ["1", "2", "3"];
+  // console.log(Array.from(String(num),Number));  //  [1, 2, 3];
+
+   //console.log(Array.from('foo'));
 // expected output: Array ["f", "o", "o"]
     //console.log(Array.from([1, 2, 3], x => x + x));
 // expected output: Array [2, 4, 6]
