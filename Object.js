bf78be02e97d0609c9102773ef647b37083dbe32
slyvochka <slivka.svitlana@gmail.com>
2020-10-28 18:23:03 -0400

    lecture for 05.06.20

--- Object.js
+++ Object.js
@@ -185,3 +185,25 @@
 // function myLanguages(results) {
 //   return Object.keys(results).filter(r => results[r] > 59).sort((a,b) => results[b] - results[a]);
 // } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
