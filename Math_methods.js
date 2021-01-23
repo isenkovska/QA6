c6a1b3e6076378b7450cd663545371e37349e8a3
slyvochka <slivka.svitlana@gmail.com>
2021-01-23 18:07:16 -0500

    Math methods

--- Math_methods.js
+++ Math_methods.js
@@ -1,6 +1,6 @@
-function hypotenuse(a, b) {
-    return Math.ceil(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
-}
+// function hypotenuse(a, b) {
+//     return Math.ceil(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
+// }
 // ex from pasv.
 // Напишите функцию с именем hypotenuse, которая принимает два числа a, b (значения катетов) в качестве аргументов и возвращает значение гипотенузы прямоугольного треугольника. Значение гипотенузы округлите вверх до ближайшего большего целого.
 
@@ -34,3 +34,23 @@ function hypotenuse(a, b) {
 //let n = 15.6;
 //console.log(Math.trunc(n));
 //console.log(n|0);      More shorter record
+
+// Codewars 
+// 7 kyu
+// https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript
+
+// Solution (1)
+//  function maxMultiple(divisor, bound){
+//     let n = divisor;
+//     for(let i = divisor; i <= bound; i += divisor ){
+//       if( i % divisor === 0 && i > n ){
+//        n = i;
+//       }
+//     }
+//     return n;
+//  }  // (2, 7) ---> 6, (3, 10) ---> 9, (10, 50) ---> 50
+
+// Solution(2)
+//  function maxMultiple(divisor, bound){
+//     return bound-bound%divisor
+//  }
