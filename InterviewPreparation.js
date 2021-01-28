d308b67fec1d8bb8d879c8a1e000e007200d0c85
slyvochka <slivka.svitlana@gmail.com>
2021-01-27 23:48:48 -0500

    useful links

--- InterviewPreparation.js
+++ InterviewPreparation.js
@@ -2,8 +2,9 @@
 // 1. https://javascript.info (english and russian)
 // 2. https://eloquent-javascript.karmazzin.ru
 
-// QA Interview questions
-//https://github.com/Vladislav610/QA_bible/blob/master/Manual%20part%201.md
+// QA Interview questions and answers
+// 1. https://github.com/Vladislav610/QA_bible/blob/master/Manual%20part%201.md
+// 2. https://www.testingxperts.com/
 
 
 
