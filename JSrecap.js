236d226e9e8728934d4a067dfbf7dc692fb505da
slyvochka <slivka.svitlana@gmail.com>
2021-01-29 12:42:00 -0500

    recap theory

---
+++ JSrecap.js
@@ -0,0 +1,12 @@
+// const animals = [
+//     {name: 'Fluffy', species: 'cat'},
+//     {name: 'Jimmy', species: 'dog'},
+//     {name: 'Pig', species: 'Quinea pig'},
+//     {name: 'Mucha', species: 'gog'},
+//     {name: 'Sparkler', species: 'cat'},
+//   ]
+
+// let onlyDogs = animals.filter(function(animal){    ---> func. like this that you send into other functions are called
+//     return animal.species === 'dog'                     CALLBACK FUNCTIONS, because the host func. will call back to them
+//   })  
+// *** filter() --> is one of the examples of high-order functions, as map() and reduce()
