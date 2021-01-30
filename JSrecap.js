819d24231eb75ee155b76471b09598361ff6e4d9
slyvochka <slivka.svitlana@gmail.com>
2021-01-30 02:46:47 -0500

    recap

--- JSrecap.js
+++ JSrecap.js
@@ -13,7 +13,9 @@
 
 
 // RECURSIVE FUNCTION calls itself untill condition met 
+// https://www.codewars.com/kata/5694cb0ec554589633000036/train/javascript
 // www.codewars.com/kata/54ff0d1f355cfd20e60001fc/solutions
+
 
 // *** Example how we can solve exercises by using Recursive fn.
 // function factorial(n) {
@@ -21,6 +23,20 @@
 //       throw new RangeError();
 //      console.log(n);
 //     return n <= 1 ? 1 : n * factorial(n - 1); //12 
-   
+
 // }
 //   console.log(factorial(4));  // 4*3*2*1=24
+
+
+// const fibonacci = n => {
+
+//     if (n <= 2) {
+//         console.log(n, 'inside if');
+//         return 1;
+       
+//     } else {
+//         console.log(n, 'inside else');
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// };
+// console.log(fibonacci(8));
