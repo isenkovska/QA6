c18789c5014c1fa9861cddc6427309381f66b30a
slyvochka <slivka.svitlana@gmail.com>
2021-02-22 19:19:38 -0500

    math exercises

---
+++ Math_exercises.js
@@ -0,0 +1,9 @@
+// A ring is an area limited by two circles that have the same center but different radius. 
+// Your task is to count if our point is lying at coordinate.
+// (the border of the ring is not included).
+//  The equation of a circle of radius r and centre the origin is   ( x2 + y2 = r2 );
+
+// function solution (inner, outer, x, y) {
+//    return ( x ** 2 + y ** 2 > inner ** 2 &&  x ** 2 + y ** 2  < outer ** 2);  
+// }
+// console.log(1, 3, 1, 0);  //Output (true)
