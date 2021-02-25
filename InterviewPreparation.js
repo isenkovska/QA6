2526611737f39a382da9987ba02954d6464eaecf
slyvochka <slivka.svitlana@gmail.com>
2021-02-25 09:11:08 -0500

    prep

--- InterviewPreparation.js
+++ InterviewPreparation.js
@@ -27,7 +27,7 @@
 // https://docs.google.com/document/d/10YxY2G35iNST-Ht1qWY-DWYRUiZtWlR4mFoE-cNZrjM/edit#heading=h.7zan7ymq7rce/
 // https://likejean.github.io/homework-5/
 // https://documenter.getpostman.com/view/11449882/SzzobG7n?version=latest#7a4f91e6-b078-4bc2-877b-8c2abb9d20c2   --> API documentation
-// https://www.ntu.edu.sg/home/ehchua/programming/webprogramming/http_basics.html  --> HTTP
+
 
 // https://docs.google.com/spreadsheets/d/18zLeN8LZDhpEH54jQAZFripHtjz2HFUCQPtfQJjKJ_4/edit#gid=0
 
