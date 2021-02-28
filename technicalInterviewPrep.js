c533f8250fb93cdf79917a6868fe9386ad2d0e7a
slyvochka <slivka.svitlana@gmail.com>
2021-02-27 22:20:12 -0500

    prep

--- technicalInterviewPrep.js
+++ technicalInterviewPrep.js
@@ -127,7 +127,23 @@
 //-----------------------------------------------------
 // https://www.codewars.com/kata/5676f07029da352ba2000065/train/javascript
 // *** Largest prime number containing n digit ***
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     for (let i = 2; i <= Math.round(num ** 0.5); i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
 
+// var largest = (n) => {
+//     if (n <= 0 || typeof (n) !== "number") return false;
+//     let number = 10 ** n - 1;
+//     while (!isPrime(number)) {
+//         number--;
+//     }
+//     return number;
+// }
+// console.log(2) ;  //Output 97
 
 
 
