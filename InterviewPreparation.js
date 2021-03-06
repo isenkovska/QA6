b845693cd05a868607c77935f50d3c3617228c66
slyvochka <slivka.svitlana@gmail.com>
2021-03-06 18:35:05 -0500

    useful links

--- InterviewPreparation.js
+++ InterviewPreparation.js
@@ -10,6 +10,7 @@
 // 2. https://www.testingxperts.com/
 // 3.  https://www.utest.com/ ---> fot real testing
 // 4. https://qxf2.com/?utm_source=qa-interview&utm_medium=click&utm_campaign=From%20QA%20Interview   ---> QA services for interview testing (Factorial)
+// 5. https://www.youtube.com/channel/UCjugV5pnY6WjvTGYm7U1ucg  ---> WebdriverIO
 
 
 // https://docs.google.com/spreadsheets/d/1o_-xifsDMBX0teiY4RPzcgcYQj8wjEiusm80cFdM480/edit?usp=sharing --> QA8(21801) Test cases "My little Hero"
